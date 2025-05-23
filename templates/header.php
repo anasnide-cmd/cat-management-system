794f08a1aada64137e4735562bf6056f23ad1263
anasnide <anasnidirproject@gmail.com>
2025-05-23 10:28:26 +0800

    Create header.php

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cat Management System</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <nav class="main-nav">
+        <a href="/">Home</a>
+        <a href="/cats/add.php">Add Cat</a>
+    </nav>
+    <div class="container">
